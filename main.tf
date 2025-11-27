b217b2991f65b1e4fada1f6319811c72a2a3ac2f
Marco Chavez <mchavez@apeseg.org.pe>
2025-11-26 22:47:25 -0500

    nuevo cambio

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data "azurerm_resource_group" "main" {
+/* data "azurerm_resource_group" "main" {
   name = "rg-cicd-terraform-app-${local.idapp}"
 }
 
@@ -79,3 +79,62 @@ resource "azurerm_role_assignment" "aca_pull_default_acr" {
 #   scope                = azurerm_container_registry.acr.id
 # }
 
+ */
+ data "azurerm_resource_group" "main" {
+  name = "rg-cicd-terraform-app-${local.idapp}"
+}
+
+data "azurerm_container_registry" "acr" {
+  name                = "acr${local.idapp}"
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+
+# Reusar el environment que ya existe
+data "azurerm_container_app_environment" "aca_env" {
+  name                = "aca-env-${local.idapp}"   # ← Ajusta con el que ya existe
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+
+resource "azurerm_container_app" "aca" {
+  name                         = "aca-ms-${local.idapp}-${var.environment}"
+  container_app_environment_id = data.azurerm_container_app_environment.aca_env.id
+  resource_group_name          = data.azurerm_resource_group.main.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "demo"
+      image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+      cpu    = 0.5
+      memory = "1Gi"
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  ingress {
+    external_enabled           = true
+    target_port                = 80
+    allow_insecure_connections = false
+
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      template[0].container[0].image,
+      registry
+    ]
+  }
+}
+
+resource "azurerm_role_assignment" "aca_pull_default_acr" {
+  principal_id         = azurerm_container_app.aca.identity[0].principal_id
+  role_definition_name = "AcrPull"
+  scope                = data.azurerm_container_registry.acr.id
+}
